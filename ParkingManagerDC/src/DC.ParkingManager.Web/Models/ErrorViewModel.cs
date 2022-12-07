69831413ef4186009b60dfb883b75da4f168b6e4
Daniel Utiyama <47907956+Utiyamo@users.noreply.github.com>
2022-12-07 09:28:55 -0300

    changes

---
+++ ParkingManagerDC/src/DC.ParkingManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DC.ParkingManager.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
